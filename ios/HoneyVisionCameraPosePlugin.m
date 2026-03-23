ab5bd19a7e23da1f3bb8fb9f446cf56496c47e38
Sam Mazzeo <sammazzeo@Sams-MacBook-Pro.local>
2026-03-23 13:08:35 -0400

    fix: add @react-native-async-storage/async-storage

--- ios/HoneyVisionCameraPosePlugin.m
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <VisionCamera/FrameProcessorPlugin.h>
-#import <VisionCamera/FrameProcessorPluginRegistry.h>
-
-#import "HoneySwingV2-Swift.h"
-
-VISION_EXPORT_SWIFT_FRAME_PROCESSOR(HoneyVisionCameraPosePlugin, honeyPoseDetect)
